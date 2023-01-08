d30cc10f57914e1c8c8e004b50712c6cfb661403
Max Merkuryev <maxmerkury13@gmail.com>
2023-01-08 17:55:56 +0700

    camera controls

---
+++ Assets/CameraControl/CameraMotion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace CameraControl {
+	public class CameraMotion : MonoBehaviour {
+		[SerializeField] private float _speed = 1f;
+		[SerializeField] private float _smoothing = 5f;
+		
+		private Vector3 _input;
+		private Vector3 _targetPosition;
+
+		private void Awake() {
+			_targetPosition = transform.position;
+		}
+			
+		private void HandleInput() {
+			float x = Input.GetAxisRaw("Horizontal");
+			float z = Input.GetAxisRaw("Vertical");
+
+			Vector3 right = transform.right * x;
+			Vector3 forward = transform.forward * z;
+
+			_input = (forward + right).normalized;
+		}
+
+		private void Move() {
+			_targetPosition += _input * _speed;
+			transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * _smoothing);
+		}
+
+		private void Update() {
+			HandleInput();
+			Move();
+		}
+	}
+}
---
+++ Assets/CameraControl/CameraZoom.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace CameraControl {
+	public class CameraZoom : MonoBehaviour{
+		[SerializeField] private Transform _cameraHolder;
+		[SerializeField] private float _smoothing = 5f;
+		[SerializeField] private float _speed = 25f;
+		[SerializeField] private Vector2 _range = new (30f, 70f);
+	
+		private float _input;
+		private Vector3 _targetPosition;
+		
+		private Vector3 _cameraDirection => transform.InverseTransformDirection(_cameraHolder.forward);
+		
+		private void Awake() {
+			_targetPosition = _cameraHolder.localPosition;
+		}
+
+		private void HandleInput() {
+			_input = Input.GetAxisRaw("Mouse ScrollWheel");
+		}
+
+		private void Zoom() {
+			_targetPosition += _cameraDirection * (_input * _speed);
+			
+			if (_targetPosition.magnitude < _range.x) _targetPosition = -_cameraDirection * _range.x;
+			if (_targetPosition.magnitude > _range.y) _targetPosition = -_cameraDirection * _range.y;
+			
+			_cameraHolder.localPosition = Vector3.Lerp(_cameraHolder.localPosition, _targetPosition, Time.deltaTime * _smoothing);
+		}
+		
+		private void Update() {
+			HandleInput();
+			Zoom();
+		}
+	}
+}
